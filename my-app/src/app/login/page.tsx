2fea57584d49cdb28ec2697a7399a80f268edd60
manhnv193 <manhnv@fit-haui.edu.vn>
2025-02-02 21:12:46 +0700

    create login page

---
+++ my-app/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+function LoginPage() {
+  
+  const message: string = "My name is Manh"
+  console.log(message)
+  
+  return (
+    <div>
+      <h1>Login</h1>
+      {/* <LoginForm /> */}
+    </div>
+  );
+}
+
+export default LoginPage;
